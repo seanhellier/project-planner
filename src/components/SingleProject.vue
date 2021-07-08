412893580657a8c3ef3f1a5f5d44f2d5c0cf2f9f
Sean Leif Hellier <sean@newtropolis.com>
2021-07-08 13:27:37 -0400

    lesson 61

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -3,8 +3,8 @@
     <div class="actions">
       <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
       <div class="icons">
-        <span class="material-icons">delete</span>
         <span class="material-icons">edit</span>
+        <span @click="deleteProject" class="material-icons">delete</span>
         <span class="material-icons tick">done</span>
       </div>
     </div>
@@ -20,8 +20,16 @@ export default {
   data() {
     return {
       showDetails: false,
+      uri: 'http://localhost:3000/projects/' + this.project.id
     }
   },
+  methods: {
+    deleteProject() {
+      fetch(uri, { method: 'DELETE' })
+        .then(() => this.$emit('delete', this.project.id))
+        .catch(err => console.log(err))
+    }
+  }
   
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,14 @@
   <div class="home">
     <div v-if="projects.length">
       <div v-for="project in projects" :key="project.id">
-        <SingleProject :project = "project" />
+        <SingleProject :project="project" @delete="handleDelete" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
-import SingleProject from "../components/SingleProject.vue"
-
+import SingleProject from '../components/SingleProject.vue'
 export default {
   name: 'Home',
   components: { SingleProject },
@@ -26,5 +24,12 @@ export default {
       .then(data => this.projects = data)
       .catch(err => console.log(err))
   },
+  methods: {
+    handleDelete(id) {
+      this.projects = this.projects.filter(project => {
+        return project.id !== id
+      })
+    }
+  }
 }
 </script>
