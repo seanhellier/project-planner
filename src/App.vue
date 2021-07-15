554a14bd69bfbd72940f3c1f3cd2db46f5926c79
Sean Leif Hellier <sean@newtropolis.com>
2021-07-14 21:30:45 -0400

    lesson 65

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
- 
+  <Navbar />
   <router-view/>
 </template>
 
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
+  components: { Navbar }
+}
+</script>
+
 <style>
 body {
-  background: #cbd0d4;
+  background: #f2f2f2;
 }
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-max-width: 600px;
-margin: 0% auto;
+  -moz-osx-font-smoothing: grayscale;
+  max-width: 600px;
+  margin: 0 auto;
   color: #555;
 }
 </style>
